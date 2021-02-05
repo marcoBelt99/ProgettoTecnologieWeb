07e4e7edb0491e5591847cf94d13233db68b2b52
Marco <marco01.beltrame@edu.unife.it>
2021-02-05 18:13:43 +0100

    File che ingloba i comandi git pull, composer install ed i vari php artisan *. Basta lanciarlo con ./script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,8 @@
 
 git pull
 cd GimmeFund
-cp .env.example .env
 composer install
-php artisan key:generate
 php artisan config:cache
-php artisan migrate
+php artisan migrate:fresh
 php artisan db:seed
 php artisan serve
