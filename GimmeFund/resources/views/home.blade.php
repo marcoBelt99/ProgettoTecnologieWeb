a0c29a7ebd47948cf7b0c7a4a8243bbbdd49aae6
Enrico <enrico.bregoli@edu.unife.it>
2021-02-15 17:20:06 +0100

    indentazione

--- GimmeFund/resources/views/home.blade.php
+++ GimmeFund/resources/views/home.blade.php
@@ -26,47 +26,40 @@
 
 @section('content')
 
-  <!-- Carousel -->
-  <div id="slider_business" class="carousel slide" data-ride="carousel">
+<!-- Carousel -->
+<div id="slider_business" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li
-        data-target="#slider_business"
-        data-slide-to="0"
-        class="active"
-      ></li>
-      <li
-        data-target="#slider_business"
-        data-slide-to="1"
-        class="active"
-      ></li>
-      <li
-        data-target="#slider_business"
-        data-slide-to="2"
-        class="active"
-      ></li>
+        <li data-target="#slider_business" data-slide-to="0" class="active">
+        </li>
+        <li data-target="#slider_business" data-slide-to="1" class="active">
+
+        </li>
+        <li data-target="#slider_business" data-slide-to="2" class="active">
+
+        </li>
     </ol>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block img-fluid" src="crowdfunding1.jpg" alt="Slide1" width="100%">
-        <div class="carousel-caption d-none d-md-block">
-          <h3>Caption per la slide 1</h3>
-          <p>Descrizione slide 1</p>
+        <div class="carousel-item active">
+            <img class="d-block img-fluid" src="crowdfunding1.jpg" alt="Slide1" width="100%">
+            <div class="carousel-caption d-none d-md-block">
+            <h3>Caption per la slide 1</h3>
+            <p>Descrizione slide 1</p>
         </div>
-      </div>
-      <div class="carousel-item">
+    </div>
+    <div class="carousel-item">
         <img class="d-block img-fluid" src="crowdfunding2.jpg" alt="Slide2" width="100%">
         <div class="carousel-caption d-none d-md-block">
           <h3>Caption per la slide 2</h3>
           <p>Descrizione slide 2</p>
         </div>
-      </div>
-      <div class="carousel-item">
+    </div>
+    <div class="carousel-item">
         <img class="d-block img-fluid" src="crowdfunding3.png" alt="Slide3" width="100%">
         <div class="carousel-caption d-none d-md-block">
           <h3>Caption per la slide 3</h3>
           <p>Descrizione slide 3</p>
         </div>
-      </div>
+    </div>
     </div>
     <a class="carousel-control-prev" href="#slider_business" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -79,11 +72,11 @@
     </div>
     <!-- Jumbotron -->
     <div class="jumbotron">
-      <h1 class="display-3">GimmeFund</h1>
-      <p class="lead">Il tuo gesto, vale doppio!</p>
-      <hr>
-      {{-- <p>Aggiungi un sottotitolo per spiegare agli utenti cosa offre l'azienda e quali sono i potenziali clienti.</p> --}}
-      <a class="btn btn-primary btn-lg btn-spl" href="#" role="button">Cosa offriamo</a>
+        <h1 class="display-3">GimmeFund</h1>
+        <p class="lead">Il tuo gesto, vale doppio!</p>
+        <hr>
+        {{-- <p>Aggiungi un sottotitolo per spiegare agli utenti cosa offre l'azienda e quali sono i potenziali clienti.</p> --}}
+        <a class="btn btn-primary btn-lg btn-spl" href="#" role="button">Cosa offriamo</a>
     </div>
     <!-- Card group -->
     <div class="container text-muted mt-3 mb-3">
@@ -132,29 +125,31 @@
     </div>
     <!-- Footer -->
     <div class="jumbotron-fluid">
-      <div class="container">
-        <h2>Sezione piè di pagina</h2>
-        <p class="lead">&copy; All Rights Reserved</p>
-      </div>
+        <div class="container">
+            <h2>Sezione piè di pagina</h2>
+            <p class="lead">&copy; All Rights Reserved</p>
+        </div>
     </div>
-  </div>
-  <!-- Al termine della pagina, prima della chiusura del tag body, si inseriscono i link alle librerie software di:  jQuery, -->
-  <script
+</div>
+@endsection
+
+@section('script')
+    <!-- Al termine della pagina, prima della chiusura del tag body, si inseriscono i link alle librerie software di:  jQuery, -->
+    <script
     src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"
-  ></script>
+    crossorigin="anonymous"></script>
 
-  <!-- Popper (per la gestione dei popup) -->
-  <script
+    <!-- Popper (per la gestione dei popup) -->
+    <script
     src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
     integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
     crossorigin="anonymous"></script>
 
-  <!-- e di Javascript-->
-  <script
+    <!-- e di Javascript-->
+    <script
     src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
     crossorigin="anonymous"></script>
-
+    
 @endsection
